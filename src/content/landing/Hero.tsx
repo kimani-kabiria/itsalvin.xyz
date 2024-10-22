f7affda60b708ef5765e534777d0943edbd38744
Kimani Kabiria <kabiriakimani@gmail.com>
2024-10-22 17:30:00 +0300

    feat: started designing the hero page

---
+++ src/content/landing/Hero.tsx
@@ -0,0 +1,15 @@
+export function Hero() {
+  return (
+    <div className="flex flex-col px-4 md:px-28 pt-32 justify-center">
+      <h1 className="text-[3em] md:text-[6em] font-bold text-center">
+        Hi, I'm Alvin 👋
+      </h1>
+      <div className="md:px-64 px-2">
+      <h2 className="text-[1.35em] md:text-[2em] font-medium text-left md:text-center">
+        An Engineer & Designer , who loves
+        crafting intelligent and beautiful looking things that live on the internet.
+      </h2>
+      </div>
+    </div>
+  );
+}
