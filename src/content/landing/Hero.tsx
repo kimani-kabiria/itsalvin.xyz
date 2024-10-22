24ae66e640c9df62139ed4aa5f55eda3e728d3af
Kimani Kabiria <kabiriakimani@gmail.com>
2024-10-22 17:33:28 +0300

    feat: added fonts to hero page

--- src/content/landing/Hero.tsx
+++ src/content/landing/Hero.tsx
@@ -1,7 +1,16 @@
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
+import clsx from "clsx";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 export function Hero() {
   return (
     <div className="flex flex-col px-4 md:px-28 pt-32 justify-center">
-      <h1 className="text-[3em] md:text-[6em] font-bold text-center">
+      <h1 className={cn("text-[3em] md:text-[6em] font-bold text-center", fontSans.variable)}>
         Hi, I'm Alvin 👋
       </h1>
       <div className="md:px-64 px-2">
