2ef71aafa8ec362252c2f2ba44814c5b190a0021
Kimani Kabiria <kabiriakimani@gmail.com>
2024-10-23 16:06:15 +0300

    fix: lint fixes on build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
-const svgToDataUri = require('mini-svg-data-uri');
+const svgToDataUri = require("mini-svg-data-uri");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -64,44 +64,46 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       keyframes: {
-        'bounce-x': {
-          '0%, 100%': {
-            transform: 'translateX(25%)',
-            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+        "bounce-x": {
+          "0%, 100%": {
+            transform: "translateX(25%)",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
           },
-          '50%': {
-            transform: 'translateX(0)',
-            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          "50%": {
+            transform: "translateX(0)",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
           },
         },
       },
     },
   },
-  plugins: [addVariablesForColors,
-    function ({ matchUtilities, theme }) {
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: { matchUtilities: any; theme: any }) {
       matchUtilities(
         {
-          'bg-grid': (value) => ({
+          "bg-grid": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}" stroke-dasharray="5 3" transform="scale(1, -1)"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
         },
-        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
       );
-    },],
+    },
+  ],
 };
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
 
   addBase({
