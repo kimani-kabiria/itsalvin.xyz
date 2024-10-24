f3c8bc1b175680c17d4a7cc57b0023b8fa4e6010
Kimani Kabiria <kabiriakimani@gmail.com>
2024-10-24 10:40:16 +0300

    fix: updated hero spacing

--- src/content/landing/Hero.tsx
+++ src/content/landing/Hero.tsx
@@ -25,7 +25,7 @@ export function Hero() {
   };
 
   return (
-    <div className="flex flex-col md:flex-row md:px-32 py-32">
+    <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row md:px-32 px-6 py-32">
       <div className="flex flex-col w-full justify-center items-start">
         <div className="flex w-full space-x-4">
           <h1 className="text-[3em] sm:text-[5.75rem] font-bold text-left font-[family-name:var(--font-acorn-bold)]">
